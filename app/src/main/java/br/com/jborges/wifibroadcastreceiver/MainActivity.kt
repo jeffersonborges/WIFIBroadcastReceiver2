5bd7890cfe3788786491f4749c21cfefebcb8340
jeffersonborges <34252717+jeffersonborges@users.noreply.github.com>
2018-02-19 00:09:08 -0300

    Criação do projeto

---
+++ app/src/main/java/br/com/jborges/wifibroadcastreceiver/MainActivity.kt
@@ -0,0 +1,16 @@
+package br.com.jborges.wifibroadcastreceiver
+
+import android.content.IntentFilter
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var filter = IntentFilter()
+        
+    }
+}
